591c26cc1531825e00eecfd1fe8e5050756f14a6
Isaac Oduh <isaacoduh@pc-200-57.customer.ask4.lan>
2023-03-01 22:21:37 +0000

    Initial Setup

---
+++ src/main/java/com/example/studentmcs/StudentmcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmcsApplication.class, args);
+	}
+
+}
