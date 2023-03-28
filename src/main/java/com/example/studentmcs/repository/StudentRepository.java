eeabbf3d17547da795668ec64f11b30f5eda7b63
Isaac Oduh <isaacoduh@pc-200-57.customer.ask4.lan>
2023-03-28 07:35:59 +0100

    create role repository, update student repository to get by email

--- src/main/java/com/example/studentmcs/repository/StudentRepository.java
+++ src/main/java/com/example/studentmcs/repository/StudentRepository.java
@@ -2,6 +2,12 @@ package com.example.studentmcs.repository;
 
 import com.example.studentmcs.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+    Boolean existByEmail(String email);
 }
