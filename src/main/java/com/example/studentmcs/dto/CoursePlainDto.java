cb1c561d73266a6e3f6c102415d8be4e09db25b9
Isaac Oduh <isaacoduh@pc-200-57.customer.ask4.lan>
2023-04-04 16:50:48 +0100

    create dtos to map course and student models

---
+++ src/main/java/com/example/studentmcs/dto/CoursePlainDto.java
@@ -0,0 +1,29 @@
+package com.example.studentmcs.dto;
+
+import com.example.studentmcs.model.Course;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class CoursePlainDto {
+    private Long id;
+    private String courseId;
+
+    private String courseTitle;
+    private String courseTerm;
+
+    public static CoursePlainDto from (Course course){
+        return CoursePlainDto.builder()
+                .id(course.getId())
+                .courseId(course.getCourseId())
+                .courseTitle(course.getCourseTitle())
+                .courseTerm(course.getCourseTerm())
+                .build();
+    }
+
+}
